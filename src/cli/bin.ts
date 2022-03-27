8fb693b255446ba08396bd868cfe4932995a676a
Thalles P <contato@thalles.me>
2022-03-27 19:57:15 -0300

    :memo: Token is now a password

--- src/cli/bin.ts
+++ src/cli/bin.ts
@@ -6,8 +6,8 @@ import { CreateDatabase } from "../lib/CreateDatabase";
 program
   .name("tempdb-cli")
   .description("CLI to create temporary database for your tests")
-  .usage("-t <token>")
-  .requiredOption("-t, --token <char>", "The token of the remote server")
+  .usage("-tp <password>")
+  .requiredOption("-p, --password <char>", "The password of the remote server")
   .usage("-h <host:port>")
   .requiredOption("-h, --host <char>", "The host of the remote server")
   .version("1.0.0")
